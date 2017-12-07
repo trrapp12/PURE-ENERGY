f08e4884f69b4afe5483dd2fb44910961629dff9
Trevor Rapp <trrapp@gmail.com>
2017-12-06 20:34:27 -0700

    fixed it.  The values getting added in were two values that both had pixel units at the end, and therefore couldn't be added as ints, so I had to parsefloat them to remove the 'px' and then add them so they would add as integers and then add the units back in

--- JS/titlebanner.js
+++ JS/titlebanner.js
@@ -16,18 +16,26 @@ function setSpacing () {
   	var purEnergyTopCoordinates = purEnergyTop.getBoundingClientRect();
  	var purEnergyTopCoordinatesCalc = window.getComputedStyle(purEnergyTop).getPropertyValue("margin-left");
  		console.log("purEnergyTopCoordinatesCalc is: " + purEnergyTopCoordinatesCalc);
+ 	var pETCC = parseFloat(purEnergyTopCoordinatesCalc)
+ 		console.log(pETCC)
  	var purEnergyTopCoordinatesPos = window.getComputedStyle(purEnergyTop).getPropertyValue("left");
  		console.log("purEnergyTopCoordinatesPos is: " + purEnergyTopCoordinatesPos);
+ 	var pETCP = parseFloat(purEnergyTopCoordinatesPos)
+ 		console.log(pETCP)
   	var counterclockwiseTopCoordinates = counterclockwiseTop.getBoundingClientRect();
 
   	console.log (sectionThreeCoordinates.bottom, sectionThreeCoordinates.height, sectionThreeCoordinates.left, sectionThreeCoordinates.right, sectionThreeCoordinates.top, sectionThreeCoordinates.width, sectionThreeCoordinates.x, sectionThreeCoordinates.y);
 
   	ourNatureTop.style.right = ((purEnergyTopCoordinates.right - purEnergyTopCoordinates.left) * .20) + "px";
 
-	var leftMargin = window.getComputedStyle(purEnergyTop, null).getPropertyValue("margin-left")
-	var x = (purEnergyTopCoordinatesPos + purEnergyTopCoordinatesCalc)
-	document.getElementById("counterclockwiseTop").style.setProperty("position", "static")
-	console.log(x)
+	var leftMargin = window.getComputedStyle(purEnergyTop, null).getPropertyValue("margin-left");
+	var x = (pETCC + pETCP) +"px";
+		console.log(x)
+	document.getElementById("counterclockwiseTop").style.setProperty("right", x);
+	// document.getElementById("counterclockwiseTop").style.right = x;
+	// document.getElementById("counterclockwiseTop").style.setAttribute("right", "200 px")
+// This worked in testing for setting the position property to static...document.getElementById("counterclockwiseTop").style.setProperty("position", "static")
+	
 }
 
 window.onload = setSpacing();
