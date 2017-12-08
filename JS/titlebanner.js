884d4a4e43220147c5a4af78c71288a61c66a2cd
Trevor Rapp <trrapp@gmail.com>
2017-12-07 18:44:33 -0700

    cleaned up the html/JS

--- JS/titlebanner.js
+++ JS/titlebanner.js
@@ -11,30 +11,33 @@ function setSpacing () {
 	var pureEnergyBottom = document.getElementById('pureEnergyBottom');
 	var ourNatureBottom = document.getElementById('ourNatureBottom');
 
-// create variable to get the bounding box information
+	var counterclockwiseTopCoordinates = counterclockwiseTop.getBoundingClientRect();
+	var leftMargin = window.getComputedStyle(purEnergyTop, null).getPropertyValue("margin-left");
  	var sectionThreeCoordinates = sectionThreeHeader.getBoundingClientRect();
+
+ 	// create variables defining the coordinates to the top, center logo.  Sets the right property of top quote "Our Nature Consists in Motion," to
   	var purEnergyTopCoordinates = purEnergyTop.getBoundingClientRect();
+  	ourNatureTop.style.position = "absolute"
+  	// ourNatureTop.style.right = (purEnergyTopCoordinates.left + 5) + "px";
+
+	// this block uses window to get the calculated "margin-left" value of the top PURE EnERGY logo, sets that equal to a variable, parses the "px" off of it, and then console logs the value
  	var purEnergyTopCoordinatesCalc = window.getComputedStyle(purEnergyTop).getPropertyValue("margin-left");
  		console.log("purEnergyTopCoordinatesCalc is: " + purEnergyTopCoordinatesCalc);
+ 		// parses the "px" off of the measurements and keeps the significant digits by using float
  	var pETCC = parseFloat(purEnergyTopCoordinatesCalc)
  		console.log(pETCC)
+
+	// this block uses window to get the calculated "left" value of the top Pure Energy logo, sets that equal to a variable, parses the "px" off of it, and then console logs the value
  	var purEnergyTopCoordinatesPos = window.getComputedStyle(purEnergyTop).getPropertyValue("left");
  		console.log("purEnergyTopCoordinatesPos is: " + purEnergyTopCoordinatesPos);
+ 	// parses the "px" off of the measurements and keeps the significant digits by using float
  	var pETCP = parseFloat(purEnergyTopCoordinatesPos)
  		console.log(pETCP)
-  	var counterclockwiseTopCoordinates = counterclockwiseTop.getBoundingClientRect();
 
-  	console.log (sectionThreeCoordinates.bottom, sectionThreeCoordinates.height, sectionThreeCoordinates.left, sectionThreeCoordinates.right, sectionThreeCoordinates.top, sectionThreeCoordinates.width, sectionThreeCoordinates.x, sectionThreeCoordinates.y);
-
-  	ourNatureTop.style.right = ((purEnergyTopCoordinates.right - purEnergyTopCoordinates.left) * .20) + "px";
-
-	var leftMargin = window.getComputedStyle(purEnergyTop, null).getPropertyValue("margin-left");
+	// This block adds the "margin-left" and the "left" value of the top PURE EnERGY logo, reintruduces the "px" unit, and sets the "right" property of "counterclockwiseTop" to that value
 	var x = (pETCC + pETCP) +"px";
 		console.log(x)
 	document.getElementById("counterclockwiseTop").style.setProperty("right", x);
-	// document.getElementById("counterclockwiseTop").style.right = x;
-	// document.getElementById("counterclockwiseTop").style.setAttribute("right", "200 px")
-// This worked in testing for setting the position property to static...document.getElementById("counterclockwiseTop").style.setProperty("position", "static")
 	
 }
 
